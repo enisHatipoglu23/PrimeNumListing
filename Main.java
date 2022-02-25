e2281236ee05088343298cbbccbc3f64fe7a314b
Enis Hatipoğlu <enishatipoglu.2001y@gmail.com>
2022-02-25 11:55:15 +0300

    PrimeNumListing
    Prime num listing from 1 to million.

---
+++ Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //
+        System.out.print("2 ");
+        for (int i = 3; i <= 1000000; i+=2){
+            System.out.print(i + " ");
+        }
+    }
+}
